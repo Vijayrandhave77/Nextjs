9f106be77bd3914108d70c3bb0abef59aeaea75a
vijay Randhave <vijayrandhave77@gmail.com>
2025-03-16 10:40:11 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
-
+import img from 'public/IMG_20220528_233021_499.jpg'
 export default function Home() {
   return (
    <div>
     <h1>My first Next js Project</h1>
+     <Image src={img} alt="img"  width={500}
+        height={500}></Image>
    </div>
   );
 }
