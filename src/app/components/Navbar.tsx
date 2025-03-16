38db6c123f14dd561d4ede4aaf0a8098e06acbf9
vijay Randhave <vijayrandhave77@gmail.com>
2025-03-16 10:17:02 +0530

    first commit

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      <ul style={{display:"flex",gap:"20px"}}>
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/contact">Contact</Link>
+        <Link href="/service">Service</Link>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
